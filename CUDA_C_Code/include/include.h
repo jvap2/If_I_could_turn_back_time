3897c5cc3b7cd7d2e3801683805ce49958072e83
jvap2 <jayvap6181@gmail.com>
2024-06-04 12:38:48 -0400

    More stuff

--- CUDA_C_Code/include/include.h
+++ CUDA_C_Code/include/include.h
@@ -1,6 +1,46 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>   
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <math.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <cusparse_v2.h>
+#include <cublas_v2.h>
+#include <cublasLt.h>
+#include <thrust/device_ptr.h>
+#include <thrust/device_vector.h>
+#include <thrust/scan.h>
+#include <thrust/execution_policy.h>
+#include <thrust/unique.h>
+#include <thrust/functional.h>
+#include <thrust/sequence.h>
+#include <thrust/inner_product.h>
+#include <thrust/transform_reduce.h>
+#include <thrust/set_operations.h>
+#include <cmath>
+#include <thrust/sort.h>
+#include <thrust/logical.h>
+#include <thrust/functional.h>
+#include <thrust/execution_policy.h>
+#include <thrust/extrema.h>
+#include <thrust/shuffle.h>
+#include <thrust/random.h>
+#include <thrust/execution_policy.h>
+#include <cusolverDn.h>
+#include "GPUErrors.h"
 
 
+#define RANGE_MAX 0.5
+#define RANGE_MIN -0.5
 
 __global__ void TiledMult(float* g_A, float* g_B, float* g_C, const int Width);
 __global__ void MatMult(float* g_A, float* g_B, float* g_C, const int ny, const int nx);
