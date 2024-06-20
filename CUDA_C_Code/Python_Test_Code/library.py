f47d872fe0d7d9fb77252272c8563e547e2554f8
jvap2 <jayvap6181@gmail.com>
2024-06-20 15:57:03 -0400

    More stuff

--- CUDA_C_Code/Python_Test_Code/library.py
+++ CUDA_C_Code/Python_Test_Code/library.py
@@ -43,10 +43,10 @@ class Linear:
         x = self.cache  # Retrieve the cached input
 
         # Compute gradient with respect to input
-        dx = np.dot(dout, self.W.T)  # Gradient of the loss w.r.t. input x
+        dx = np.dot(dout, self.W.T)  # Gradient of the loss w.r.t. input x, delta* W^T
 
         # Compute gradient with respect to weights
-        dW = np.dot(x.T, dout)  # Gradient of the loss w.r.t. weights W
+        dW = np.dot(x.T, dout)  # Gradient of the loss w.r.t. weights W, delta* x^T(activation of previous layer)
 
         # Compute gradient with respect to biases
         db = np.sum(dout, axis=0, keepdims=True)  # Gradient of the loss w.r.t. biases b
@@ -57,6 +57,81 @@ class Linear:
         self.b -= learning_rate * db  # Update biases
 
         return dx  # Return the gradient with respect to input for the next layer in backpropagation
+
+class Conv2D:
+    def __init__(self, num_filters, filter_size, input_channels):
+        self.num_filters = num_filters
+        self.filter_size = filter_size
+        self.input_channels = input_channels
+        self.W = np.random.randn(filter_size, filter_size, input_channels, num_filters) * 0.01  # Initialize weights
+        self.b = np.zeros((1, 1, 1, num_filters))  # Initialize biases
+        self.cache = None  # To store the input and other necessary variables for backward pass
+
+    def forward(self, x):
+        self.cache = x  # Cache the input x
+        # Add the forward pass implementation here (not shown for brevity)
+        # Example: out = convolution_operation(x, self.W, self.b)
+        # self.cache = (x, self.W, self.b)  # Cache necessary values
+        # return out
+        filter_size, num_filters = self.filter_size, self.num_filters
+
+        # Retrieve dimensions
+        n_H_prev, n_W_prev, n_C_prev = x.shape[1], x.shape[2], x.shape[3]
+        n_H = n_H_prev - filter_size + 1
+        n_W = n_W_prev - filter_size + 1
+
+        # Initialize output
+        out = np.zeros((x.shape[0], n_H, n_W, num_filters))
+
+        # Compute the convolution
+
+        for i in range(n_H):
+            for j in range(n_W):
+                for c in range(num_filters):
+                    h_start = i
+                    h_end = h_start + filter_size
+                    w_start = j
+                    w_end = w_start + filter_size
+
+                    out[:, i, j, c] = np.sum(x[:, h_start:h_end, w_start:w_end, :] * self.W[:, :, :, c] + self.b[:, :, :, c], axis=(1, 2, 3))
+        
+        return out  # Return the output
+
+    def backward(self, dout):
+        x = self.cache  # Retrieve the cached input
+        filter_size, num_filters = self.filter_size, self.num_filters
+
+        # Initialize gradients
+        dx = np.zeros_like(x)
+        dW = np.zeros_like(self.W)
+        db = np.zeros_like(self.b)
+
+        # Retrieve dimensions
+        n_H_prev, n_W_prev, n_C_prev = x.shape[1], x.shape[2], x.shape[3]
+        n_H, n_W, n_C = dout.shape[1], dout.shape[2], dout.shape[3]
+
+        # Compute gradients
+        for i in range(n_H):
+            for j in range(n_W):
+                for c in range(n_C):
+                    # Compute the gradient with respect to the input (dx)
+                    h_start = i
+                    h_end = h_start + filter_size
+                    w_start = j
+                    w_end = w_start + filter_size
+
+                    dx[:, h_start:h_end, w_start:w_end, :] += self.W[:, :, :, c] * dout[:, i, j, c][:, None, None, None]
+
+                    # Compute the gradient with respect to the weights (dW)
+                    dW[:, :, :, c] += np.sum(x[:, h_start:h_end, w_start:w_end, :] * dout[:, i, j, c][:, None, None, None], axis=0)
+
+                    # Compute the gradient with respect to the biases (db)
+                    db[:, :, :, c] += np.sum(dout[:, i, j, c], axis=0)
+
+        return dx, dW, db  # Return the gradients
+
+
+
     
 class NeuralNetwork:
     def __init__(self, layers):
@@ -72,3 +147,26 @@ class NeuralNetwork:
             dout = layer.backward(dout)
         return dout
 
+
+
+# Define the network architecture
+layers = [
+    Linear(input_dim=4, output_dim=5),
+    ReLU(),
+    Linear(input_dim=5, output_dim=3),
+    Sigmoid()
+]
+
+# Create the neural network
+nn = NeuralNetwork(layers)
+
+# Forward pass
+x = np.random.randn(10, 4)  # Example input
+output = nn.forward(x)
+
+# # Compute loss (example, assuming some loss function)
+# loss = compute_loss(output, y)
+
+# # Backward pass
+# dout = compute_loss_gradient(output, y)  # Gradient of loss w.r.t. output
+# nn.backward(dout)
