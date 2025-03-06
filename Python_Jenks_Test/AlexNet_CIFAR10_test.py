1e481080891ad1809fb0fda1a4647c3e1e28fa8e
jvap2 <jayvap6181@gmail.com>
2025-03-06 15:40:46 -0500

    More nonsense

--- Python_Jenks_Test/AlexNet_CIFAR10_test.py
+++ Python_Jenks_Test/AlexNet_CIFAR10_test.py
@@ -11,6 +11,46 @@ import os
 import torch.nn as nn
 from networks import LeNet5V1,alexnet
 
+torch.cuda.empty_cache()
+# train_val_dataset = datasets.MNIST(root="./datasets/", train=True, download=True)
+# test_dataset = datasets.MNIST(root="./datasets/", train=False, download=True)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+model = nn.Sequential(
+                nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+                nn.BatchNorm2d(96),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2),
+
+                nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2),
+
+                nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(384),
+                nn.ReLU(),
+
+                nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(384),
+                nn.ReLU(),
+
+                nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 3, stride = 2),
+
+                nn.Flatten(),
+
+                nn.Linear(9216, 4096),
+                nn.ReLU(),
+
+                nn.Linear(4096, 4096),
+                nn.ReLU(),
+
+                nn.Linear(4096, 10)
+            ).to(device)
 
 timestamp = datetime.now().strftime("%Y-%m-%d")
 experiment_name = "MNIST"
